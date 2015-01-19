16d6f7547d3739ad230a3531d351c6d14991cdd4
shinyquagsire23 <mtinc2@gmail.com>
2015-01-19 13:46:40 -0800

    Added compile.sh, moved shared dlplay code into shared.s

---
+++ compile.sh
@@ -0,0 +1,4 @@
+#1/bin/bash
+armips spider_rop.s
+armips spider_rop_4x.s
+armips spider_rop_5x_6x.s
--- spider_rop.s
+++ spider_rop.s
@@ -166,43 +166,7 @@ spiderRop:
 		.word 0x00000008 ; flags
 		.word 0x00000000 ; unused
 
-	.align 0x4
-	dlplayCode:
-		ldr r0, =DLPLAY_NSSHANDLE_LOC_VA ; ns:s handle location
-		ldr r0, [r0]
-
-		mrc p15, 0, r1, c13, c0, 3
-		add r1, 0x80
-		ldr r2, =0x00100180 ; NSS:RebootSystem
-		str r2, [r1], #4
-		ldr r2, =0x00000001 ; flag
-		str r2, [r1], #4
-		ldr r2, =0x00000000 ; lower word PID (0 for gamecard)
-		str r2, [r1], #4
-		ldr r2, =0x00000000 ; upper word PID
-		str r2, [r1], #4
-		ldr r2, =0x00000002 ; mediatype (2 for gamecard)
-		str r2, [r1], #4
-		ldr r2, =0x00000000 ; reserved
-		str r2, [r1], #4
-		ldr r2, =0x00000000 ; flag
-		str r2, [r1], #4
-
-		.word 0xef000032 ; svc 0x32 (sendsyncrequest)
-
-		;sleep forever and ever...
-		ldr r0, =0xFFFFFFFF
-		ldr r1, =0x0FFFFFFF
-		.word 0xef00000a ; svc 0xa (sleep)
-
-		.pool
-	dlplayCode_end:
-
-	.align 0x4
-	dlplayHook:
-		.word DLPLAY_CODE_LOC_VA, DLPLAY_CODE_LOC_VA, DLPLAY_CODE_LOC_VA, DLPLAY_CODE_LOC_VA
-		.word DLPLAY_CODE_LOC_VA, DLPLAY_CODE_LOC_VA
-	dlplayHook_end:
+.include "shared.s"
 
 .org 0x1DFFF
 .byte 0x0
